2846b5b82301ddfe3c4225c2350c662e14d39f0b
Brennan B <bdbasinger777@gmail.com>
2018-06-28 14:20:26 +0000

    automatically backed up by learn

--- splice.js
+++ splice.js
@@ -13,6 +13,11 @@ console.log(items) // [2,4,6,8]
 
 var items = [1,3,5,7,9,11,13,15,17,19]
 
-items.splice(4,3) // At index 4, remove 3 items
 
-console.log(items)
+
+console.log(items.splice(4,3)) // At index 4, remove 3 items
+// removes [9,11,13] from the items array and returns these elements
+
+
+console.log(items) // [ 1, 3, 5, 7, 15, 17, 19 ]
+
