cae217838fec407751d45b09571b7620d6351705
Brennan B <bdbasinger777@gmail.com>
2018-06-28 13:27:17 +0000

    automatically backed up by learn

--- splice.js
+++ splice.js
@@ -1,3 +1,9 @@
 var items = [2,4,6,8,10,12,14,16]
 
+items.splice(4) 
+//removes everything after index 4 from the array 
+//and returns the items that have been removed
+
+console.log(items)
+
 console.log(items.splice(1))
