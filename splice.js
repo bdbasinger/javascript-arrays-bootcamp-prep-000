9724726105ba7bc60ee96e279f264207ba1568e1
Brennan B <bdbasinger777@gmail.com>
2018-06-28 14:24:22 +0000

    automatically backed up by learn

--- splice.js
+++ splice.js
@@ -1,9 +1,15 @@
-//The .splice Method
+//The .splice() Method
 
+
+
+
+
+// .splice(a) with one argument
+// At index a,remove all items after and including index a
 var items = [2,4,6,8,10,12,14,16]
 
 items.splice(4) // [10,12,14,16]
-//removes everything after index 4 from the items array 
+//removes everything after and including index 4 from the items array 
 //and returns the items that have been removed [10,12,14,16]
 
 console.log(items) // [2,4,6,8]
@@ -11,13 +17,44 @@ console.log(items) // [2,4,6,8]
 
 
 
+
+
+
+
+
+// .splice(a,b) with two arguments
+// At index a, remove b items
+
 var items = [1,3,5,7,9,11,13,15,17,19]
 
-
-
-items.splice(4,3) // At index 4, remove 3 items
+items.splice(4,3) 
+// At index 4, remove 3 items
 // removes [9,11,13] from the items array and returns these elements
 
-
 console.log(items) // [ 1, 3, 5, 7, 15, 17, 19 ]
+
+
+
+
+
+
+
+
+
+
+// .splice() with three arguments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
